874b8830486b7a1288c364a7091e8cefa04e2761
Sumit Das <sudas@purestorage.com>
2022-10-29 02:04:56 +0530

    add: `null_resource` for px validation
    Signed-off-by: Sumit Das <sudas@purestorage.com>

--- main.tf
+++ main.tf
@@ -38,14 +38,17 @@ resource "helm_release" "portworx" {
     name  = "aut"
     value = true
   }
-
+}
+resource "null_resource" "validate_portworx_installation" {
   provisioner "local-exec" {
     command     = "bash portworx_wait_untill_ready.sh"
     working_dir = "${path.module}/utils"
     interpreter = ["/bin/bash", "-c"]
   }
+  depends_on = [
+    helm_release.portworx
+  ]
 }
-
 
 resource "kubernetes_manifest" "storageclass_default" {
   manifest = {
@@ -122,7 +125,7 @@ resource "helm_release" "portworx_backup" {
     }
   }
   depends_on = [
-    helm_release.portworx,
+    null_resource.validate_portworx_installation,
     kubernetes_manifest.storageclass_default
   ]
 }
