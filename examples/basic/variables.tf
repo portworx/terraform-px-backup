45f7ea1cbf11797e53632da21c51b009c0beb042
Sumit Das <sudas@purestorage.com>
2022-11-02 22:20:04 +0530

    update: updated Basic example (examples/basic)
    Signed-off-by: Sumit Das <sudas@purestorage.com>

--- examples/basic/variables.tf
+++ examples/basic/variables.tf
@@ -16,12 +16,6 @@ variable "px_operator_version" {
   default     = "1.10.0"
 }
 
-variable "namespace" {
-  type        = string
-  description = "Namespace to deploy PX-Central"
-  default     = "central"
-}
-
 variable "kubecontext" {
   type        = string
   description = "Name of the kubecontext"
@@ -31,62 +25,4 @@ variable "kubeconfig_path" {
   type        = string
   description = "Path to the kubeconfig to connect to the kubernetes cluster"
   default     = "~/.kube/config"
-}
-
-variable "enable_px_license_server" {
-  type        = bool
-  description = "Enable Portworx License Server"
-  default     = false
-}
-
-variable "enable_portworx" {
-  type        = bool
-  description = "Enable Portworx Storage Cluster"
-  default     = true
-}
-
-variable "external_oidc_provider" {
-  type = object({
-    endpoint      = string
-    client_id     = string
-    client_secret = string
-  })
-  description = "Configuration for external OIDC Provider: Endpoint, Client ID and Client Secret"
-  default     = null
-}
-
-variable "custom_image_registry" {
-  type = object({
-    image_registry     = string
-    image_repository   = string
-    image_pull_secrets = list(string)
-  })
-  description = "Configuration for Custom Image Registry: Image Registry, Image Repository, Image Pull Secrets"
-  default     = null
-}
-
-variable "portworx_storage_class_config" {
-  type = object({
-    name        = string
-    replication = number
-    provisioner = string
-  })
-  description = "Configuration for Default Portworx Storage Class: Name, Replications, Provisioner"
-  default     = null
-}
-variable "px_monitor_config" {
-  type = object({
-    enable                 = bool
-    px_central_ui_endpoint = string
-  })
-  description = "Configuration for Portworx Monitor: enable, Px-Central UI's Endpoint"
-  default = {
-    enable                 = false
-    px_central_ui_endpoint = ""
-  }
-}
-variable "external_storage_class_name" {
-  type        = string
-  description = "Name of the already existing Storage Class to be used in-place of `portworx_storage_class_config`"
-  default     = ""
 }
