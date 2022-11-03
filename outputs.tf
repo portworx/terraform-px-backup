b76c6268144a8d9aa07e4a8f0965381ccd3eec06
Sumit Das <sudas@purestorage.com>
2022-11-03 13:16:35 +0530

    update: updated outputs descriptions
    Signed-off-by: Sumit Das <sudas@purestorage.com>

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,9 @@
-# TODO: Create proper outputs and add descriptions
 output "portworx_operator_status" {
   value       = helm_release.portworx[0].metadata[0]
-  description = "value"
+  description = "Portworx Storage Class Parameters"
 }
 
 output "portworx_backup_status" {
   value       = helm_release.portworx_backup.metadata[0]
-  description = "value"
+  description = "Portworx Backup Parameters"
 }
