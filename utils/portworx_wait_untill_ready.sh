a83f75da145d1b7b82a3198286665e05c4df5c6e
Sumit Das <sudas@purestorage.com>
2022-12-03 16:56:36 +0530

    Add: Wait for all PX Pods to come up
    Signed-off-by: Sumit Das <sudas@purestorage.com>

--- utils/portworx_wait_untill_ready.sh
+++ utils/portworx_wait_untill_ready.sh
@@ -6,8 +6,7 @@ RETRIES=0
 
 sleep 120
 
-while [ "$RETRIES" -le "$LIMIT" ]
-do
+while [ "$RETRIES" -le "$LIMIT" ]; do
     STATUS=$(kubectl get storagecluster px-cluster -n kube-system -o jsonpath='{.status.phase}')
     if [ "$STATUS" == "Online" ]; then
         CLUSTER_ID=$(kubectl get storagecluster px-cluster -n kube-system -o jsonpath='{.status.clusterUid}')
@@ -16,6 +15,25 @@ do
     fi
     printf "[INFO] Portworx Storage Cluster Status: [ $STATUS ]\n"
     printf "[INFO] Waiting for Portworx Storage Cluster. (Retry in $SLEEP_TIME secs)\n"
+    ((RETRIES++))
+    sleep $SLEEP_TIME
+done
+
+if [ "$RETRIES" -gt "$LIMIT" ]; then
+    printf "[ERROR] All Retries Exhausted!\n"
+    exit 1
+fi
+
+RETRIES=0
+PODS_COUNT=$(k get pods -l name=portworx -n kube-system | wc -l | xargs)
+while [ "$RETRIES" -le "$LIMIT" ]; do
+    READY_COUNT=$(k get pods -l name=portworx -n kube-system -o json | jq -r '.items[] | select(.status.conditions[].type=="Ready") | .metadata.name' | wc -l | xargs)
+    if [ "$PODS_COUNT" == "$READY_COUNT" ]; then
+        printf "[SUCCESS] All Portworx Pods are up and running\n"
+        break
+    fi
+    printf "[INFO] Portworx Pods Status: [ $READY_COUNT/$PODS_COUNT ]\n"
+    printf "[INFO] Waiting for All Portworx Pods. (Retry in $SLEEP_TIME secs)\n"
     ((RETRIES++))
     sleep $SLEEP_TIME
 done
