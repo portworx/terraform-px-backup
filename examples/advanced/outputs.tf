4c05a62b5af08997c4a88bf08cc7384c0714a23c
Sumit Das <sudas@purestorage.com>
2022-12-06 11:44:27 +0530

    Add: Dynamic Indexing
    Signed-off-by: Sumit Das <sudas@purestorage.com>

--- examples/advanced/outputs.tf
+++ examples/advanced/outputs.tf
@@ -1,4 +1,4 @@
-output "installation_status" {
-  value       = var.enable_portworx_backup ? module.portworx_backup.portworx_backup_status : null
-  description = "Portworx Backup Status"
-}
+# output "installation_status" {
+#   value       = var.enable_portworx_backup ? module.portworx_backup.portworx_backup_status : null
+#   description = "Portworx Backup Status"
+# }
