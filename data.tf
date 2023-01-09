3f5c2df865088290e934e223c0db540f671f3db3
Sumit Das <sudas@purestorage.com>
2023-01-09 20:00:12 +0530

    Add Drives and KVDB Device Parameter for EKS
    Signed-off-by: Sumit Das <sudas@purestorage.com>

--- data.tf
+++ data.tf
@@ -3,5 +3,5 @@ locals {
   custom_image_registry_config = var.custom_image_registry != null ? ["pxcentralApiServerImage", "pxcentralFrontendImage", "pxcentralBackendImage", "pxcentralMiddlewareImage", "postInstallSetupImage", "keycloakBackendImage", "keycloakFrontendImage", "keycloakLoginThemeImage", "keycloakInitContainerImage", "mysqlImage", "pxBackupImage", "mongodbImage", "licenseServerImage", "cortexImage", "cassandraImage", "proxyConfigImage", "consulImage", "dnsmasqImage", "grafanaImage", "prometheusImage", "prometheusConfigReloadrImage", "prometheusOperatorImage", "memcachedMetricsImage", "memcachedIndexImage", "memcachedImage"] : []
   px_monitor_config            = var.px_monitor_config.enable ? { "pxmonitor.enabled" = "true", "installCRDs" = "true", "pxmonitor.pxCentralEndpoint" = var.px_monitor_config.px_central_ui_endpoint } : { "pxmonitor.enabled" = "false" }
   px_backup_custom_image       = var.px_backup_custom_image != null ? ["pxBackupImage"] : []
-  eks_install_config           = var.provisioner == "eks" ? { "drives" : "type=gp2,size=150;type=gp2,size=150;type=gp2,size=150", "kvdbDevice" : "type=gp2,size=100" } : {}
+  eks_install_config           = var.provisioner == "eks" ? { "drives" = "type=gp2,size=150;type=gp2,size=150;type=gp2,size=150", "kvdbDevice" = "type=gp2,size=100" } : {}
 }
